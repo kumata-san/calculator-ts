b7aa0d8161963a89b3ee5fd0536a7a061459d7d4
kumata-san <kumatasan2019@gmail.com>
2020-01-05 23:29:26 +0900

    second commit

---
+++ src/components/NumBtn.tsx
@@ -0,0 +1,8 @@
+import React, { FC, useContext } from "react";
+import Context from "./Context";
+import { onNumClick } from "../reducers";
+const NumBtn: FC<{ n: number }> = ({ n }) => {
+  const { dispatch } = useContext(Context);
+  return <button onClick={() => dispatch(onNumClick(n))}>{n}</button>;
+};
+export default NumBtn;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useReducer } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Context from "./components/Context";
+import { initialAppState, reducer } from "./reducers";
+
+const Index = () => {
+  const [state, dispatch] = useReducer(reducer, initialAppState);
+  return (
+    <Context.Provider value={{ state, dispatch }}>
+      <App />
+    </Context.Provider>
+  );
+};
+
+ReactDOM.render(<Index />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
